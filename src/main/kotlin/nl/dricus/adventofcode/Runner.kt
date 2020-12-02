78890b2f2cccfdafe72f357adc2660d061f4f90a
Dirk Groot <dirk.inge@gmail.com>
2020-12-02 23:02:36 +0100

    A tiny bit more refactoring

--- src/main/kotlin/nl/dricus/adventofcode/Runner.kt
+++ src/main/kotlin/nl/dricus/adventofcode/Runner.kt
@@ -1,16 +1,14 @@
 package nl.dricus.adventofcode
 
+import nl.dricus.adventofcode.util.Puzzle
+
 fun main(args: Array<String>) {
     val year = args[0].toInt()
     val day = args[1]
 
     if (day == "all")
         puzzles[year]?.forEachIndexed { index, puzzle ->
-            puzzle.apply {
-                println("Solutions for puzzle $year-${index + 1}:")
-                println("- Part 1: ${part1()}")
-                println("- Part 2: ${part2()}")
-            }
+            printSolution(year, index + 1, puzzle)
         }
     else
         solve(year, day.toInt())
@@ -22,9 +20,11 @@ private fun solve(year: Int, day: Int) {
     if (puzzle == null)
         println("Could't find a puzzle for year $year, day $day.")
     else
-        puzzle.apply {
-            println("Solutions for puzzle $year-$day:")
-            println("- Part 1: ${part1()}")
-            println("- Part 2: ${part2()}")
-        }
+        printSolution(year, day, puzzle)
+}
+
+private fun printSolution(year: Int, day: Int, puzzle: Puzzle) {
+    println("Solutions for puzzle $year-$day:")
+    println("- Part 1: ${puzzle.part1()}")
+    println("- Part 2: ${puzzle.part2()}")
 }
