c5ec15451acdf268b8e204e983e8591264597a46
Dirk Groot <dirk.inge@gmail.com>
2020-12-06 08:30:08 +0100

    Day 6

--- src/main/kotlin/nl/dricus/adventofcode/Puzzles.kt
+++ src/main/kotlin/nl/dricus/adventofcode/Puzzles.kt
@@ -3,5 +3,5 @@ package nl.dricus.adventofcode
 import nl.dricus.adventofcode.year2020.*
 
 val puzzles = mapOf(
-    2020 to arrayOf(Day01, Day02, Day03, Day04, Day05)
+    2020 to arrayOf(Day01, Day02, Day03, Day04, Day05, Day06)
 )
---
+++ src/main/kotlin/nl/dricus/adventofcode/year2020/Day06.kt
@@ -0,0 +1,33 @@
+package nl.dricus.adventofcode.year2020
+
+import nl.dricus.adventofcode.util.Input
+import nl.dricus.adventofcode.util.Puzzle
+
+object Day06 : Puzzle() {
+    private class Person(val yesAnswers: Set<Char>)
+    private class Group(val persons: List<Person>)
+
+    private val groups by lazy {
+        Input.string(2020, 6)
+            .split("\n\n")
+            .map {
+                Group(it.split("\n")
+                    .map { personAnswers -> Person(personAnswers.toSet()) })
+            }
+    }
+
+    override fun part1() =
+        groups
+            .map { it.persons.flatMap { person -> person.yesAnswers }.toSet() }
+            .fold(0) { sum, groupAnswers -> sum + groupAnswers.size }
+
+    override fun part2(): Int =
+        groups
+            .map {
+                val allQuestions = ('a'..'z').toSet()
+                it.persons.fold(allQuestions) { allPersonsYes, current ->
+                    allPersonsYes intersect current.yesAnswers
+                }
+            }
+            .fold(0) { sum, current -> sum + current.size }
+}
