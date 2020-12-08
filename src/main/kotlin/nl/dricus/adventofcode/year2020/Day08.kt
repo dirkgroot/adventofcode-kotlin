484107ea65103a2cf0334fd1ca5ab69be3e94947
Dirk Groot <d.groot@avisi.nl>
2020-12-08 09:40:00 +0100

    Day 8 - refactoring

--- src/main/kotlin/nl/dricus/adventofcode/year2020/Day08.kt
+++ src/main/kotlin/nl/dricus/adventofcode/year2020/Day08.kt
@@ -8,35 +8,34 @@ object Day08 : Puzzle() {
     private val program by lazy { parseProgram(Input.lines(2020, 8)) }
 
     private fun parseProgram(code: List<String>) = code.map {
-        val tokens = it.split(" ")
-        tokens[0] to tokens[1].toInt()
+        it.split(" ").let { (instruction, argument) -> instruction to argument.toInt() }
     }
 
     override fun part1() = run().second
 
     override fun part2() = program.asSequence().withIndex()
-        .filter { it.value.first in setOf("nop", "jmp") }
-        .map { run(it.index) }
-        .filter { (isLoop, _) -> !isLoop }
-        .map { (_, acc) -> acc }
-        .first()
+        .filter { it.value.first != "acc" }
+        .map { run(flip = it.index) }
+        .first { (isLoop, _) -> !isLoop }
+        .let { (_, acc) -> acc }
 
-    private tailrec fun run(
-        flip: Int = -1, ip: Int = 0, acc: Int = 0, visited: Set<Int> = emptySet()
-    ): Pair<Boolean, Int> =
+    private fun run(flip: Int = -1, ip: Int = 0, acc: Int = 0, visited: Set<Int> = emptySet()): Pair<Boolean, Int> =
         when {
             ip >= program.size -> false to acc
             visited.contains(ip) -> true to acc
             else -> {
-                val (instruction, argument) = program[ip]
-                when (instruction to (ip == flip)) {
-                    "acc" to false -> run(flip, ip + 1, acc + argument, visited + ip)
-                    "jmp" to false -> run(flip, ip + argument, acc, visited + ip)
-                    "nop" to true -> run(flip, ip + argument, acc, visited + ip)
-                    "nop" to false -> run(flip, ip + 1, acc, visited + ip)
-                    "jmp" to true -> run(flip, ip + 1, acc, visited + ip)
+                val (instruction, argument) = instruction(ip, ip == flip)
+                when (instruction) {
+                    "acc" -> run(flip, ip + 1, acc + argument, visited + ip)
+                    "jmp" -> run(flip, ip + argument, acc, visited + ip)
+                    "nop" -> run(flip, ip + 1, acc, visited + ip)
                     else -> throw IllegalStateException()
                 }
             }
         }
+
+    private fun instruction(index: Int, flip: Boolean) = program[index].let { (instruction, argument) ->
+        if (flip) (if (instruction == "jmp") "nop" else "jmp") to argument
+        else instruction to argument
+    }
 }
