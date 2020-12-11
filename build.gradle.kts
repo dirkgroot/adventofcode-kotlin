6223cd34a17a9cc52397c9029ce0676ef7e00c2d
Dirk Groot <dirk.inge@gmail.com>
2020-12-11 17:50:31 +0100

    Enable running unit tests with Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,5 +11,16 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+
     testImplementation(kotlin("test-junit5"))
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.2.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.2.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-console:1.2.0")
+}
+
+tasks {
+    "test"(Test::class) {
+        useJUnitPlatform()
+    }
 }
