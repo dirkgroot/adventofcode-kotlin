48df231752880e8f0b28755d6cd6b15b5e635a58
Dirk Groot <dirk.inge@gmail.com>
2020-12-12 08:19:43 +0100

    Day 12 - refactoring

--- src/main/kotlin/nl/dricus/adventofcode/year2020/Day12.kt
+++ src/main/kotlin/nl/dricus/adventofcode/year2020/Day12.kt
@@ -8,43 +8,39 @@ import kotlin.math.abs
 class Day12(input: Input) : Puzzle() {
     private val instructions by lazy { input.lines().map { it[0] to it.substring(1).toInt() } }
 
-    class State(val waypoint: Pair<Int, Int> = 1 to 0, val posX: Int = 0, val posY: Int = 0) {
-        val manhattanDistance = abs(posX) + abs(posY)
+    class State(val waypoint: Pair<Int, Int>, val ship: Pair<Int, Int> = 0 to 0) {
+        val manhattanDistance = abs(ship.first) + abs(ship.second)
     }
 
-    override fun part1() = followInstructions(State(), ::moveShip).manhattanDistance
+    override fun part1() = followInstructions(State(waypoint = 1 to 0), ::moveShip).manhattanDistance
 
     override fun part2() = followInstructions(State(waypoint = 10 to 1), ::moveWaypoint).manhattanDistance
 
     private fun followInstructions(initialState: State, moveCommand: (State, Char, Int) -> State) =
         instructions.fold(initialState) { state, (action, value) ->
-            val (dirX, dirY) = state.waypoint
+            val (waypointX, waypointY) = state.waypoint
+            val (shipX, shipY) = state.ship
             when (action) {
-                'F' -> State(state.waypoint, state.posX + dirX * value, state.posY + dirY * value)
-                'R' -> State(rotate(dirX, dirY, value), state.posX, state.posY)
-                'L' -> State(rotate(dirX, dirY, -value), state.posX, state.posY)
+                'F' -> State(state.waypoint, shipX + waypointX * value to shipY + waypointY * value)
+                'R' -> State(rotate(waypointX, waypointY, value), state.ship)
+                'L' -> State(rotate(waypointX, waypointY, -value), state.ship)
                 else -> moveCommand(state, action, value)
             }
         }
 
     private fun moveShip(state: State, direction: Char, value: Int) =
-        when (direction) {
-            'N' -> State(state.waypoint, state.posX, state.posY + value)
-            'E' -> State(state.waypoint, state.posX + value, state.posY)
-            'S' -> State(state.waypoint, state.posX, state.posY - value)
-            'W' -> State(state.waypoint, state.posX - value, state.posY)
-            else -> throw IllegalStateException("Invalid direction: ($direction, $value)")
-        }
+        state.ship.let { (x, y) -> State(state.waypoint, move(x, y, direction, value)) }
 
     private fun moveWaypoint(state: State, direction: Char, value: Int) =
-        state.waypoint.let { (dirX, dirY) ->
-            when (direction) {
-                'N' -> State(dirX to dirY + value, state.posX, state.posY)
-                'E' -> State(dirX + value to dirY, state.posX, state.posY)
-                'S' -> State(dirX to dirY - value, state.posX, state.posY)
-                'W' -> State(dirX - value to dirY, state.posX, state.posY)
-                else -> throw IllegalStateException("Invalid direction: ($direction, $value)")
-            }
+        state.waypoint.let { (x, y) -> State(move(x, y, direction, value), state.ship) }
+
+    private fun move(x: Int, y: Int, direction: Char, value: Int) =
+        when (direction) {
+            'N' -> x to y + value
+            'E' -> x + value to y
+            'S' -> x to y - value
+            'W' -> x - value to y
+            else -> throw IllegalStateException("Invalid direction: ($direction, $value)")
         }
 
     private tailrec fun rotate(dirX: Int, dirY: Int, degrees: Int): Pair<Int, Int> =
