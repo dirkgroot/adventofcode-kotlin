385e5ea2841510d2ef2e3a655a2089ae14f85747
Dirk Groot <dirk.inge@gmail.com>
2020-12-14 22:25:40 +0100

    Day 14 - a little refactoring

--- src/main/kotlin/nl/dricus/adventofcode/year2020/Day14.kt
+++ src/main/kotlin/nl/dricus/adventofcode/year2020/Day14.kt
@@ -17,38 +17,34 @@ class Day14(input: Input) : Puzzle() {
         }
     }
 
-    private fun parseMask(mask: String): SetMask {
-        val maskValue = mask.drop(7)
+    private fun parseMask(mask: String) = mask.drop(7).let { maskValue ->
         val xMask = maskValue.replace('1', '0').replace('X', '1')
         val nMask = maskValue.replace('X', '0')
 
-        return SetMask(xMask.toLong(2), nMask.toLong(2))
+        SetMask(xMask.toLong(2), nMask.toLong(2))
     }
 
-    private fun parseMem(mem: String): Instruction {
-        val match = "mem\\[(\\d+)] = (\\d+)".toRegex().matchEntire(mem)!!
-
-        return SetMem(match.groupValues[1].toLong(), match.groupValues[2].toLong())
+    private fun parseMem(mem: String) = "mem\\[(\\d+)] = (\\d+)".toRegex().matchEntire(mem)!!.let { match ->
+        SetMem(match.groupValues[1].toLong(), match.groupValues[2].toLong())
     }
 
     override fun part1() = sumOfMemory(::setMemWithMaskedValue)
 
     private fun setMemWithMaskedValue(setMask: SetMask, setMem: SetMem) =
-        listOf(SetMem(setMem.address, (setMem.value and setMask.xMask) or setMask.nMask))
+        sequenceOf(SetMem(setMem.address, (setMem.value and setMask.xMask) or setMask.nMask))
 
     override fun part2() = sumOfMemory(::setMemPermutations)
 
     private fun setMemPermutations(mask: SetMask, instruction: SetMem) =
         (instruction.address and mask.xMask.inv())
-            .let { fixedBits ->
-                generateSequence(mask.xMask) { if (it > 0) (it - 1) and mask.xMask else null }
-                    .map { fixedBits or mask.nMask or it }
-                    .toList()
-            }
+            .let { fixedBits -> addressPermutations(mask).map { address -> fixedBits or mask.nMask or address } }
             .map { SetMem(it, instruction.value) }
 
-    private fun sumOfMemory(setMemInstructions: (SetMask, SetMem) -> List<SetMem>) = program
-        .runningFold(SetMask(0, 0) to listOf<SetMem>()) { (mask, previous), next ->
+    private fun addressPermutations(mask: SetMask) =
+        generateSequence(mask.xMask) { prev -> if (prev > 0) (prev - 1) and mask.xMask else null }
+
+    private fun sumOfMemory(setMemInstructions: (SetMask, SetMem) -> Sequence<SetMem>) = program
+        .runningFold(SetMask(0, 0) to sequenceOf<SetMem>()) { (mask, previous), next ->
             when (next) {
                 is SetMask -> next to previous
                 is SetMem -> mask to setMemInstructions(mask, next)
