cbaefbf04261957071ea7c5f4b969a351a1d0941
Dirk Groot <dirk.inge@gmail.com>
2020-12-14 21:47:29 +0100

    Day 14 - and now it's all functional :D

--- src/main/kotlin/nl/dricus/adventofcode/year2020/Day14.kt
+++ src/main/kotlin/nl/dricus/adventofcode/year2020/Day14.kt
@@ -31,48 +31,32 @@ class Day14(input: Input) : Puzzle() {
         return SetMem(match.groupValues[1].toLong(), match.groupValues[2].toLong())
     }
 
-    override fun part1(): Long {
-        var mask = SetMask(0, 0)
-        val memory = mutableMapOf<Long, Long>()
+    override fun part1() = sumOfMemory(::setMemWithMaskedValue)
 
-        program.forEach { instruction ->
-            when (instruction) {
-                is SetMask -> mask = instruction
-                is SetMem -> memory[instruction.address] = applyValueMask(mask.xMask, mask.nMask, instruction.value)
+    private fun setMemWithMaskedValue(setMask: SetMask, setMem: SetMem) =
+        listOf(SetMem(setMem.address, (setMem.value and setMask.xMask) or setMask.nMask))
+
+    override fun part2() = sumOfMemory(::setMemPermutations)
+
+    private fun setMemPermutations(mask: SetMask, instruction: SetMem) =
+        (instruction.address and mask.xMask.inv())
+            .let { fixedBits ->
+                generateSequence(mask.xMask) { if (it > 0) (it - 1) and mask.xMask else null }
+                    .map { fixedBits or mask.nMask or it }
+                    .toList()
+            }
+            .map { SetMem(it, instruction.value) }
+
+    private fun sumOfMemory(setMemInstructions: (SetMask, SetMem) -> List<SetMem>) = program
+        .runningFold(SetMask(0, 0) to listOf<SetMem>()) { (mask, previous), next ->
+            when (next) {
+                is SetMask -> next to previous
+                is SetMem -> mask to setMemInstructions(mask, next)
+                else -> throw IllegalStateException()
             }
         }
-
-        return memory.values.sum()
-    }
-
-    private fun applyValueMask(xMask: Long, nMask: Long, value: Long) = (value and xMask) or nMask
-
-    override fun part2(): Long {
-        var mask = SetMask(0, 0)
-        val memory = mutableMapOf<Long, Long>()
-
-        program.forEach { instruction ->
-            when (instruction) {
-                is SetMask -> mask = instruction
-                is SetMem -> getAddressPermutations(mask.xMask, mask.nMask, instruction.address).forEach {
-                    memory[it] = instruction.value
-                }
-            }
-        }
-
-        return memory.values.sum()
-    }
-
-    private fun getAddressPermutations(xMask: Long, nMask: Long, address: Long): List<Long> {
-        val fixedBits = address and xMask.inv()
-        val result = mutableListOf<Long>()
-        var orPermutation = xMask
-
-        do {
-            result.add(fixedBits or nMask or orPermutation)
-            orPermutation = (orPermutation - 1) and xMask
-        } while (orPermutation != xMask)
-
-        return result
-    }
+        .flatMap { (_, instructions) -> instructions }
+        .groupBy { it.address }
+        .entries.map { (_, instructions) -> instructions.last().value }
+        .sum()
 }
