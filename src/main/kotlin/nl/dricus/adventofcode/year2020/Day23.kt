8e1dc26815c665cc6b05034732f0d3944ffdea4d
Dirk Groot <dirk.inge@gmail.com>
2020-12-23 15:30:53 +0100

    Day 23 - more optimization

--- src/main/kotlin/nl/dricus/adventofcode/year2020/Day23.kt
+++ src/main/kotlin/nl/dricus/adventofcode/year2020/Day23.kt
@@ -6,64 +6,51 @@ import nl.dricus.adventofcode.util.Puzzle
 class Day23(val input: Input) : Puzzle() {
     private val order by lazy { input.string().map { it - '0' - 1 } }
 
-    private class Cup(val label: Int, next: Cup? = null) {
-        var next: Cup = next ?: this
-        override fun toString() = "label=$label"
-    }
-
     override fun part1(): String {
-        val index = createIndex(order, 9)
+        val index = createIndex2(order, 9)
         play(index, order[0], 100)
-        return createPart1Result(index[0].next)
+        return createPart1Result(index, index[0])
     }
 
-    private tailrec fun createPart1Result(head: Cup, acc: String = ""): String =
-        if (head.label == 0) acc
-        else createPart1Result(head.next, acc + (head.label + 1))
+    private tailrec fun createPart1Result(index: IntArray, head: Int, acc: String = ""): String =
+        if (head == 0) acc
+        else createPart1Result(index, index[head], acc + (head + 1))
 
     override fun part2(): Long {
-        val index = createIndex(order, 1_000_000)
+        val index = createIndex2(order, 1_000_000)
         play(index, order[0], 10_000_000)
-        return (index[0].next.label.toLong() + 1) * (index[0].next.next.label.toLong() + 1)
+        return (index[0].toLong() + 1) * (index[index[0]].toLong() + 1)
     }
 
-    private fun createIndex(order: List<Int>, size: Int): Array<Cup> {
-        val result = Array(size) { index -> Cup(index) }
-        result.forEachIndexed { index, cup ->
-            val orderIndex = order.indexOf(cup.label)
-            if (orderIndex in 0..order.lastIndex)
-                cup.next = result[order[(orderIndex + 1) % order.size]]
-            else
-                cup.next = result[(index + 1) % size]
-        }
-        if (size > order.size) {
-            result[order.last()].next = result[order.size]
-            result.last().next = result[order[0]]
+    private fun createIndex2(initialCups: List<Int>, size: Int): IntArray {
+        val allCups = IntArray(size) { index -> if (index < initialCups.size) initialCups[index] else index }
+        val result = IntArray(size)
+        allCups.forEachIndexed { index, label ->
+            result[label] = allCups[(index + 1) % allCups.size]
         }
         return result
     }
 
-    private fun play(index: Array<Cup>, first: Int, turns: Int) {
+    private fun play(index: IntArray, first: Int, turns: Int) {
         fun previousLabel(label: Int) = (label + index.size - 1) % index.size
 
-        var current = index[first]
+        var current = first
         repeat(turns) {
-            val pick1 = current.next
-            val pick2 = pick1.next
-            val pick3 = pick2.next
+            val pick1 = index[current]
+            val pick2 = index[pick1]
+            val pick3 = index[pick2]
 
-            var destinationLabel = previousLabel(current.label)
-            while (pick1.label == destinationLabel || pick2.label == destinationLabel || pick3.label == destinationLabel) {
+            var destinationLabel = previousLabel(current)
+            while (pick1 == destinationLabel || pick2 == destinationLabel || pick3 == destinationLabel) {
                 destinationLabel = previousLabel(destinationLabel)
             }
 
-            val destination = index[destinationLabel]
-            val old = destination.next
-            destination.next = pick1
-            current.next = pick3.next
-            pick3.next = old
+            val old = index[destinationLabel]
+            index[destinationLabel] = pick1
+            index[current] = index[pick3]
+            index[pick3] = old
 
-            current = current.next
+            current = index[current]
         }
     }
 }
