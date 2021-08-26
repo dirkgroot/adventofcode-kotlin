35b50df4e52376bcfb632ccffb2d925fc633c5a5
Dirk Groot <d.groot@avisi.nl>
2021-08-26 08:28:29 +0200

    2019 Day 2 - Use persistent collection from kotlinx-collections-immutable

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.4.10"
+    kotlin("jvm") version "1.5.21"
 }
 
 group = "nl.dricus"
@@ -11,12 +11,13 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.4")
 
     testImplementation(kotlin("test-junit5"))
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.2.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.2.0")
-    testRuntimeOnly("org.junit.platform:junit-platform-console:1.2.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-console:1.7.2")
 }
 
 tasks {
--- src/main/kotlin/nl/dricus/adventofcode/year2019/Day02.kt
+++ src/main/kotlin/nl/dricus/adventofcode/year2019/Day02.kt
@@ -1,36 +1,41 @@
 package nl.dricus.adventofcode.year2019
 
+import kotlinx.collections.immutable.PersistentList
+import kotlinx.collections.immutable.toPersistentList
 import nl.dricus.adventofcode.util.Input
 import nl.dricus.adventofcode.util.Puzzle
 
-class Day02(input: Input, replaceInput: Boolean = true) : Puzzle() {
-    data class Line(val instruction: (Int, Int) -> Int, val input1: Int, val input2: Int)
-
-    private val program = if (replaceInput)
-        readProgram(input)
-            .mapIndexed { index, s ->
-                when (index) {
-                    1 -> 12
-                    2 -> 2
-                    else -> s
-                }
-            }
-    else
-        readProgram(input)
-
-    private fun readProgram(input: Input) = input.string()
+class Day02(input: Input, private val replaceInput: Boolean = true) : Puzzle() {
+    private val program = input.string()
         .split(",")
         .map(String::toInt)
 
-    private val lines = program
-        .chunked(4)
-        .takeWhile { it[0] != 99 }
-        .associate { it[3] to Line(if (it[0] == 1) Int::plus else Int::times, it[1], it[2]) }
+    override fun part1() = if (replaceInput)
+        exec(program, 12, 2)
+    else
+        exec(program, program[1], program[2])
 
-    override fun part1() = valueAt(0)
+    override fun part2() =
+        allNounVerbCombos()
+            .first { (noun, verb) -> exec(program, noun, verb) == 19690720 }
+            .let { (noun, verb) -> 100 * noun + verb }
 
-    private fun valueAt(index: Int): Int =
-        lines[index]
-            ?.let { it.instruction(valueAt(it.input1), valueAt(it.input2)) }
-            ?: program[index]
+    private fun exec(program: List<Int>, noun: Int, verb: Int): Int {
+        tailrec fun exec(memory: PersistentList<Int>, ip: Int = 0): Int =
+            when (memory[ip]) {
+                1 -> exec(memory.set(memory[ip + 3], memory[memory[ip + 1]] + memory[memory[ip + 2]]), ip + 4)
+                2 -> exec(memory.set(memory[ip + 3], memory[memory[ip + 1]] * memory[memory[ip + 2]]), ip + 4)
+                else -> memory[0]
+            }
+
+        return exec(program.toPersistentList().set(1, noun).set(2, verb))
+    }
+
+    private fun allNounVerbCombos() = generateSequence(0 to 0) { (noun, verb) ->
+        when {
+            noun == 100 -> null
+            verb == 99 -> noun + 1 to 0
+            else -> noun to verb + 1
+        }
+    }
 }
