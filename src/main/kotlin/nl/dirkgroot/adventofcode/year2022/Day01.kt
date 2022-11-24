21e4525282353bba19403777c6132a67d50cc166
Dirk Groot <dirkgroot77@gmail.com>
2022-11-24 18:50:22 +0100

    Year 2022, day 1 preparation

---
+++ src/main/kotlin/nl/dirkgroot/adventofcode/year2022/Day01.kt
@@ -0,0 +1,7 @@
+package nl.dirkgroot.adventofcode.year2022
+
+import nl.dirkgroot.adventofcode.util.Input
+import nl.dirkgroot.adventofcode.util.Puzzle
+
+class Day01(input: Input) : Puzzle() {
+}
---
+++ src/test/kotlin/nl/dirkgroot/adventofcode/year2022/Solutions2022Test.kt
@@ -0,0 +1,10 @@
+package nl.dirkgroot.adventofcode.year2022
+
+import nl.dirkgroot.adventofcode.base.AbstractYearSolutionsTest
+import nl.dirkgroot.adventofcode.util.ClasspathResourceInput
+
+class Solutions2022Test : AbstractYearSolutionsTest() {
+    override val testCases = listOf(
+        TestCase(Day01(ClasspathResourceInput(2022, 1))),
+    )
+}
