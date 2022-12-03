e26454cd7015ed2bc469be7405ffa5c7197383fc
Dirk Groot <dirkgroot77@gmail.com>
2022-12-03 18:29:40 +0100

    Year 2022, day 1 - Switch to Kotest and put solutions and tests in 1 file

--- src/test/kotlin/nl/dirkgroot/adventofcode/year2022/Day01Test.kt
+++ src/test/kotlin/nl/dirkgroot/adventofcode/year2022/Day01Test.kt
@@ -1,35 +1,41 @@
 package nl.dirkgroot.adventofcode.year2022
 
-import assertk.assertThat
-import assertk.assertions.isEqualTo
-import nl.dirkgroot.adventofcode.util.StringInput
-import org.junit.jupiter.api.Test
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import nl.dirkgroot.adventofcode.util.input
+import nl.dirkgroot.adventofcode.util.invokedWith
 
-class Day01Test {
-    @Test
-    fun `example input`() {
-        val day01 = Day01(
-            StringInput(
-                """
-                    1000
-                    2000
-                    3000
-    
-                    4000
-    
-                    5000
-                    6000
-    
-                    7000
-                    8000
-                    9000
-    
-                    10000
-                """.trimIndent()
-            )
-        )
+private const val YEAR = 2022
+private const val DAY = 1
 
-        assertThat(day01.part1()).isEqualTo(24000L)
-        assertThat(day01.part2()).isEqualTo(45000L)
-    }
-}
+private fun solution1(input: String) = calories(input).max()
+private fun solution2(input: String) = calories(input).sortedDescending().take(3).sum()
+
+private fun calories(input: String) = input.split("\n\n")
+    .map { elf -> elf.split("\n").map { calories -> calories.toLong() } }
+    .map { elf -> elf.sum() }
+
+class Day01Test : StringSpec({
+    "example part 1" { ::solution1 invokedWith exampleInput shouldBe 24000L }
+    "part 1 solution" { ::solution1 invokedWith input(YEAR, DAY) shouldBe 69836L }
+    "example part 2" { ::solution2 invokedWith exampleInput shouldBe 45000L }
+    "part 2 solution" { ::solution2 invokedWith input(YEAR, DAY) shouldBe 207968L }
+})
+
+private val exampleInput =
+    """
+        1000
+        2000
+        3000
+        
+        4000
+        
+        5000
+        6000
+        
+        7000
+        8000
+        9000
+        
+        10000
+    """.trimIndent()
