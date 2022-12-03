d22577a0b4a59af0ded17e0fcadcabe26ef24539
Dirk Groot <dirkgroot77@gmail.com>
2022-12-03 18:16:58 +0100

    Create template for solutions

---
+++ src/test/kotlin/nl/dirkgroot/adventofcode/year2022/Day00Test.kt
@@ -0,0 +1,23 @@
+package nl.dirkgroot.adventofcode.year2022
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import nl.dirkgroot.adventofcode.util.input
+import nl.dirkgroot.adventofcode.util.invokedWith
+
+private const val YEAR = 2022
+private const val DAY = 0
+
+private fun solution1(input: String): Long = 0L
+private fun solution2(input: String): Long = 0L
+
+class Day00Test : StringSpec({
+    "example part 1" { ::solution1 invokedWith exampleInput shouldBe 0L }
+    "part 1 solution" { ::solution1 invokedWith input(YEAR, DAY) shouldBe 0L }
+    "example part 2" { ::solution2 invokedWith exampleInput shouldBe 0L }
+    "part 2 solution" { ::solution2 invokedWith input(YEAR, DAY) shouldBe 0L }
+})
+
+private val exampleInput =
+    """
+    """.trimIndent()
