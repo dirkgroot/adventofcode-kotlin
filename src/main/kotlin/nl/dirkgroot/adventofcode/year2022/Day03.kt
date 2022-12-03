4fca08cb64f4b01acc4223f431bb02713277fc1a
Dirk Groot <dirkgroot77@gmail.com>
2022-12-03 06:38:56 +0100

    Year 2022, day 3 - Use chunked instead of windowed

--- src/main/kotlin/nl/dirkgroot/adventofcode/year2022/Day03.kt
+++ src/main/kotlin/nl/dirkgroot/adventofcode/year2022/Day03.kt
@@ -7,12 +7,12 @@ class Day03(input: Input) : Puzzle() {
     private val rucksacks = input.lines()
 
     override fun part1() = rucksacks.map { rucksack ->
-        rucksack.windowed(rucksack.length / 2, rucksack.length / 2)
+        rucksack.chunked(rucksack.length / 2)
             .map { it.toSet() }
             .reduce { acc, half -> acc.intersect(half) }
     }.sumOf { priority(it.single()) }
 
-    override fun part2() = rucksacks.windowed(3, 3).map { group ->
+    override fun part2() = rucksacks.chunked(3).map { group ->
         group.map { rucksack -> rucksack.toSet() }
             .reduce { acc, rucksack -> acc.intersect(rucksack) }
     }.sumOf { priority(it.single()) }
