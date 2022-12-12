fc3d52189a703316ee11e8aa032cb9c01161c85d
Dirk Groot <dirkgroot77@gmail.com>
2022-12-12 13:28:55 +0100

    Year 2022, day 12 - optimize performance using priority queue

--- src/test/kotlin/nl/dirkgroot/adventofcode/year2022/Day12Test.kt
+++ src/test/kotlin/nl/dirkgroot/adventofcode/year2022/Day12Test.kt
@@ -4,44 +4,41 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import nl.dirkgroot.adventofcode.util.input
 import nl.dirkgroot.adventofcode.util.invokedWith
-import java.util.PriorityQueue
+import java.io.File
+import java.util.*
 
-private fun solution1(input: String) = Grid(input).let { it.shortest(sequenceOf(it.startX to it.startY)) }
+private fun solution1(input: String) = Grid(input).let { it.shortest(sequenceOf(it.start)) }
 private fun solution2(input: String) = Grid(input).let { it.shortest(it.vertices { c -> c == 'a' }) }
 
 private class Grid(input: String) {
     private val infinity = 100000000
-    private val cells = input.lineSequence().map { it.toCharArray() }.toList()
-    private val endX = input.filter { it != '\n' }.indexOf('E') % cells[0].size
-    private val endY = input.filter { it != '\n' }.indexOf('E') / cells[0].size
-
-    val startX = input.filter { it != '\n' }.indexOf('S') % cells[0].size
-    val startY = input.filter { it != '\n' }.indexOf('S') / cells[0].size
+    private val cells = input.filter { it != '\n' }.toCharArray()
+    private val height = input.lineSequence().count()
+    private val width = cells.size / height
+    val start = cells.indexOf('S')
+    private val end = cells.indexOf('E')
 
     init {
-        cells[startY][startX] = 'a'
-        cells[endY][endX] = 'z'
+        cells[start] = 'a'
+        cells[end] = 'z'
     }
 
-    fun vertices(predicate: (Char) -> Boolean) = sequence {
-        for (y in cells.indices) for (x in cells[y].indices)
-            if (predicate(cells[y][x])) yield(x to y)
-    }
+    fun vertices(predicate: (Char) -> Boolean) = cells.indices.asSequence().filter { predicate(cells[it]) }
 
-    fun shortest(starts: Sequence<Pair<Int, Int>>): Int {
-        val dist = mutableMapOf<Pair<Int, Int>, Int>()
-        val prev = mutableMapOf<Pair<Int, Int>, Pair<Int, Int>>()
-        val q = PriorityQueue<Pair<Int, Int>> { o1, o2 -> dist.getValue(o1).compareTo(dist.getValue(o2)) }
+    fun shortest(starts: Sequence<Int>): Int {
+        val dist = mutableMapOf<Int, Int>()
+        val prev = mutableMapOf<Int, Int>()
+        val q = PriorityQueue<Int> { o1, o2 -> dist.getValue(o1).compareTo(dist.getValue(o2)) }
 
-        for (y in cells.indices) for (x in cells[y].indices) dist[x to y] = infinity
+        cells.indices.forEach { dist[it] = infinity }
         starts.forEach { v -> dist[v] = 0 }
         q.addAll(starts)
 
         while (q.isNotEmpty()) {
             val u = q.poll()
-            if (u == endX to endY || dist[u] == infinity) break
+            if (u == end || dist[u] == infinity) break
 
-            neighbors(u.first, u.second).forEach { v ->
+            neighbors(u).forEach { v ->
                 val alt = dist.getValue(u) + 1
                 if (alt < dist.getValue(v)) {
                     dist[v] = alt
@@ -51,13 +48,15 @@ private class Grid(input: String) {
             }
         }
 
-        return dist.getValue(endX to endY)
+        return dist.getValue(end)
     }
 
-    private fun neighbors(x: Int, y: Int) =
-        sequenceOf(x - 1 to y, x + 1 to y, x to y - 1, x to y + 1)
-            .filter { (x, y) -> x in cells[0].indices && y in cells.indices }
-            .filter { (nx, ny) -> cells[ny][nx] - cells[y][x] <= 1 }
+    private fun neighbors(index: Int) = sequence {
+        if (index % width > 0) yield(index - 1)
+        if (index % width < width - 1) yield(index + 1)
+        if (index + width < cells.size) yield(index + width)
+        if (index >= width) yield(index - width)
+    }.filter { cells[it] - cells[index] <= 1 }
 }
 
 //===============================================================================================\\
